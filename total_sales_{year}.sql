312d1fc7d411d783dac7b97599ac0d580fa735af
Frankie Bruno <frankiebruno1991@gmail.com>
2023-05-23 14:17:54 -0500

    exercise 11

---
+++ total_sales_{year}.sql
@@ -0,0 +1,7 @@
+SELECT '2009' AS Year, COUNT(*) AS InvoiceCount, SUM(Total)
+FROM Invoice
+WHERE InvoiceDate LIKE '2009%'
+UNION ALL
+SELECT '2011' AS Year, COUNT(*) AS InvoiceCount, SUM(Total)
+FROM Invoice
+WHERE InvoiceDate LIKE '2011%';
